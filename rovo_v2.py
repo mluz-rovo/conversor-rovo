5ec894c8d00ae746571feb4f099b7ec773aadf21
mluz-rovo <m.luz@ronzinovonoswald.com>
2026-04-06 10:15:34 +0100

    Update rovo_v2.py

--- rovo_v2.py
+++ rovo_v2.py
@@ -161,3 +161,9 @@ if arquivo:
     except Exception as e:
         st.error(f"Erro: {e}")
         st.exception(e)
+
+elif arquivo.name.endswith(".pdf") and cliente == "Studio Nicholson":
+    with pdfplumber.open(arquivo) as pdf:
+        for i, page in enumerate(pdf.pages):
+            st.subheader(f"Página {i+1} — Texto raw")
+            st.text(page.extract_text())
